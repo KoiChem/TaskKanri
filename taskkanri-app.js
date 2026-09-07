c88aaaf63c1b859da4e36428b8e9d3e3bf6f49e2
“koike” <taiyaki.taiyaki.taiyaki@gmail.com>
2026-09-07 11:48:46 +0900

    Use calendar space for larger date cells

--- taskkanri-app.js
+++ taskkanri-app.js
@@ -26,7 +26,7 @@ import {
   serializePayload,
   stripHtml,
   weekdayForDateId
-} from './taskkanri-core.mjs?v=20260907-bulk-calendar-layout-v4';
+} from './taskkanri-core.mjs?v=20260907-bulk-calendar-layout-v5';
 
 const APP_CONFIG = CORE_CONFIG;
 const PERIOD_SLOTS = new Set(['１限', '２限', '３限', '４限', '５限', '６限', '７限']);
